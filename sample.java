eff2475eacdc3c4b55cc8ccefa95c75d42fb9818
unknown <mridulchourasia18@gmail.com>
2025-04-08 12:49:36 +0530

    this is sample

---
+++ sample.java
@@ -0,0 +1,21 @@
+public class sample {
+    public static void main(String[] args) {
+        // Original int value
+        int intValue = 42;
+        System.out.println("Original int value: " + intValue);
+        
+        // Convert int to float
+        float floatValue = intValue; // Implicit conversion
+        
+        // Print both values
+        System.out.println("Converted float value: " + floatValue);
+        
+        // Example of another conversion
+        double doubleValue = floatValue; // Implicit conversion
+        System.out.println("Converted double value: " + doubleValue);
+        
+
+        //new question 
+        
+    }
+}
